78144770b793d8bc2812a5bf8773a78198c2c470
Y-Ysss <y-ysss@outlook.com>
2020-08-31 23:21:41 +0900

    Add(*): Add dynamic generation of theme options

--- js/common.js
+++ js/common.js
@@ -1,108 +1,118 @@
 const asyncFunc = callbackFunc => (...args) => new Promise((resolve, reject) => {
-  callbackFunc(...args, result => {
-    if (chrome.runtime.lastError) {
-      reject(new Error(chrome.runtime.lastError.message));
-      return;
-    }
-    resolve(result);
-  });
+    callbackFunc(...args, result => {
+        if(chrome.runtime.lastError) {
+            reject(new Error(chrome.runtime.lastError.message));
+            return;
+        }
+        resolve(result);
+    });
 });
 
 const getStorage = asyncFunc((keys, callback) => {
-  chrome.storage.local.get(keys, callback);
+    chrome.storage.local.get(keys, callback);
 });
 const setStorage = asyncFunc((keys, callback) => {
-  chrome.storage.local.set(keys, callback);
+    chrome.storage.local.set(keys, callback);
 });
 const getBookmarksTree = asyncFunc((callback) => {
-	chrome.bookmarks.getTree(callback);
+    chrome.bookmarks.getTree(callback);
 });
 const getBookmarkItems = asyncFunc((keys, callback) => {
-  chrome.bookmarks.get(keys, callback)
+    chrome.bookmarks.get(keys, callback)
 })
 const wrapper = (key, action, func) => {
-  const all = document.querySelectorAll(key)
-  for(const item of all) {
-    item.addEventListener(action, (event) => {func(event)})
-  }
+    const all = document.querySelectorAll(key)
+    for(const item of all) {
+        item.addEventListener(action, (event) => { func(event) })
+    }
 }
 
 class DefaultSettings {
-  constructor() {
-    this.settings = {
-      "toggle": {"tgglIcon": false, "tgglOpenTab": true, "tgglWebSearch":false, "tgglAutoTheme":false},
-      "radio": {"theme": "tmFlatLight"},
-      "text": { "txtScale": "", "txtRegexpPattern":"", "txtMacyColumns":"", "txtMacyMarginX":"", "txtMacyBreak":""},
-      "range":{ "sliderLower": "", "sliderUpper": ""},
-      "select": {"autoThemePrimary": "tmFlatLight", "autoThemeSecondary": "tmFlatDark"},
-      "format_version": "0.1"
+    constructor() {
+        this.settings = {
+            "toggle": { "tgglIcon": false, "tgglOpenTab": true, "tgglWebSearch": false, "tgglAutoTheme": false },
+            "radio": { "theme": "tmFlatLight" },
+            "text": { "txtScale": "", "txtRegexpPattern": "", "txtMacyColumns": "", "txtMacyMarginX": "", "txtMacyBreak": "" },
+            "range": { "sliderLower": "", "sliderUpper": "" },
+            "select": { "autoThemePrimary": "tmFlatLight", "autoThemeSecondary": "tmFlatDark" },
+            "format_version": "0.1"
+        }
+        this.themes = [
+            { "id": "tmLight", "label": "Modern Light" },
+            { "id": "tmDark", "label": "Modern Dark" },
+            { "id": "tmOrange", "label": "Modern Orange" },
+            { "id": "tmFlatLight", "label": "Flat Light" },
+            { "id": "tmFlatDark", "label": "Flat Dark" },
+            { "id": "tmFullFlatLight", "label": "FullFlat Light" },
+            { "id": "tmStylishWhite", "label": "Stylish White" },
+            { "id": "tmStylishDark", "label": "Stylish Dark" },
+        ]
+        this.loadData()
     }
-    this.loadData()
-  }
-  async loadData() {
-    const data = await getStorage(null)
-    if(data.settings !== undefined && data.settings.format_version === this.settings.format_version){
-      this.settings = data.settings
-    } else {
-      this.saveData()
+    async loadData() {
+        const data = await getStorage(null)
+        if(data.settings !== undefined && data.settings.format_version === this.settings.format_version) {
+            this.settings = data.settings
+        } else {
+            this.saveData()
+        }
+        this.init()
     }
-    this.init()
-  }
-  async saveData() {
-    await setStorage({'settings': this.settings})
-  }
-  init(){}
+    async saveData() {
+        await setStorage({ 'settings': this.settings })
+    }
+    init() {}
 
-  // ------------------------Debug
-  formatTime(date) {
-    const year_str = date.getFullYear();
-    const month_str = 1 + date.getMonth();
-    const day_str = date.getDate();
-    const hour_str = date.getHours();
-    const minute_str = date.getMinutes();
-    const second_str = date.getSeconds();
-    let format_str = 'YYYY-MM-DD hh:mm:ss';
-    format_str = format_str.replace(/YYYY/g, year_str);
-    format_str = format_str.replace(/MM/g, month_str);
-    format_str = format_str.replace(/DD/g, day_str);
-    format_str = format_str.replace(/hh/g, hour_str);
-    format_str = format_str.replace(/mm/g, ('0' + minute_str).slice(-2));
-    format_str = format_str.replace(/ss/g, ('0' + second_str).slice(-2));
-    return format_str;
-  }
+    // ------------------------Debug
+    formatTime(date) {
+        const year_str = date.getFullYear();
+        const month_str = 1 + date.getMonth();
+        const day_str = date.getDate();
+        const hour_str = date.getHours();
+        const minute_str = date.getMinutes();
+        const second_str = date.getSeconds();
+        let format_str = 'YYYY-MM-DD hh:mm:ss';
+        format_str = format_str.replace(/YYYY/g, year_str);
+        format_str = format_str.replace(/MM/g, month_str);
+        format_str = format_str.replace(/DD/g, day_str);
+        format_str = format_str.replace(/hh/g, hour_str);
+        format_str = format_str.replace(/mm/g, ('0' + minute_str).slice(-2));
+        format_str = format_str.replace(/ss/g, ('0' + second_str).slice(-2));
+        return format_str;
+    }
 
-  async autoTheme() {
-    const data = await getStorage('settings')
-    this.settings.range = data.settings.range
-    let t1 = data.settings.range.sliderLower;
-    let t2 = data.settings.range.sliderUpper;
-    const now = new Date()
-    console.log(this.formatTime(now))
-    const h = now.getHours()
-    let tm
-    if(t1 <= t2) {
-      if(t1 <= h && h < t2) {
-        console.log('theme1')
-        tm = data.settings.select.autoThemePrimary
-      } else if(h < t1 || t2 <= h) {
-        console.log('theme2')
-        tm = data.settings.select.autoThemeSecondary
-      }
-    } else if(t2 < t1) {
-      if(t2 <= h && h < t1) {
-        console.log('theme2')
-        tm = data.settings.select.autoThemeSecondary
-      }else if(t1 <= h || h < t2) {
-        console.log('theme1')
-        tm = data.settings.select.autoThemePrimary
-      }
+    async autoTheme() {
+        const data = await getStorage('settings')
+        this.settings.range = data.settings.range
+        let t1 = data.settings.range.sliderLower;
+        let t2 = data.settings.range.sliderUpper;
+        const now = new Date()
+        console.log(this.formatTime(now))
+        const h = now.getHours()
+        let tm
+        if(t1 <= t2) {
+            if(t1 <= h && h < t2) {
+                console.log('theme1')
+                tm = data.settings.select.autoThemePrimary
+            } else if(h < t1 || t2 <= h) {
+                console.log('theme2')
+                tm = data.settings.select.autoThemeSecondary
+            }
+        } else if(t2 < t1) {
+            if(t2 <= h && h < t1) {
+                console.log('theme2')
+                tm = data.settings.select.autoThemeSecondary
+            } else if(t1 <= h || h < t2) {
+                console.log('theme1')
+                tm = data.settings.select.autoThemePrimary
+            }
+        }
+        if(this.settings.radio.theme !== tm) {
+            this.settings.radio.theme = tm
+            console.log('tm:', tm)
+            this.saveData()
+            chrome.runtime.sendMessage({ newtab: 'reload' })
+            chrome.runtime.sendMessage({ option: 'reload' })
+        }
     }
-    if(this.settings.radio.theme !== tm) {
-      this.settings.radio.theme = tm
-      console.log('tm:', tm)
-      this.saveData()
-      chrome.runtime.sendMessage({newtab: 'reload'})
-      chrome.runtime.sendMessage({option: 'reload'})
-    }
-  }
 }
--- js/option.js
+++ js/option.js
@@ -1,163 +1,190 @@
 class Reflector {
-	static toggle(key, value) {
-		if(value) {
-			document.getElementById(key).classList.add('toggle-on')
-		}
-	}
-	static text(key, value) {
-		document.getElementById(key).value = value
-	}
-	static range(key, value) {
-		for(const item of document.querySelectorAll(`.${key}`)) {
-			item.value = value
-		}
-		document.getElementById(`${key}Range`).value = value
-	}
-	static radio(key, value) {
-		document.getElementById(value).checked = true
-	}
-	static select(key, value) {
-		for(const item of document.querySelectorAll(`select[name="${key}"]`)) {
-			item.value = value
-		}
-	}
+    static toggle(key, value) {
+        if(value) {
+            document.getElementById(key).classList.add('toggle-on')
+        }
+    }
+    static text(key, value) {
+        document.getElementById(key).value = value
+    }
+    static range(key, value) {
+        for(const item of document.querySelectorAll(`.${key}`)) {
+            item.value = value
+        }
+        document.getElementById(`${key}Range`).value = value
+    }
+    static radio(key, value) {
+        document.getElementById(value).checked = true
+    }
+    static select(key, value) {
+        for(const item of document.querySelectorAll(`select[name="${key}"]`)) {
+            item.value = value
+        }
+    }
 }
 
 class ReflectSettings extends DefaultSettings {
-	constructor() {
-		super()
-	}
-	init() {
-		this.reflect()
-		this.addElementsEventListener()
-	}
-	reflect() {
-		const data = this.settings
-		console.log(data)
-		for(const type in data){
-			if(typeof data[type] === "object") {
-				this.setState(type, data[type])
-			}
-		}
-	}
-	setState(type, data) {
-		for(const key in data) {
-			Reflector[type](key, data[key])
-		}
-	}
-	wrapper(key, action, func) {
-		const all = document.querySelectorAll(key)
-		for(const item of all) {
-			item.addEventListener(action, (event) => {func(event)})
-		}
-	}
+    constructor() {
+        super()
+    }
+    init() {
+        this.addThemeOptions()
+        this.reflect()
+        this.addElementsEventListener()
+    }
+    addThemeOptions() {
+        const radioFragment = document.createDocumentFragment()
+        const option1Fragment = document.createDocumentFragment()
+        const option2Fragment = document.createDocumentFragment()
+        const inputBase = document.createElement('input')
+        const labelBase = document.createElement('label')
+        const optionBase = document.createElement('option')
+        const themes = this.themes
+        for(const theme of themes) {
+            const inpt = inputBase.cloneNode()
+            const labl = labelBase.cloneNode()
+            const optn1 = optionBase.cloneNode()
+            const optn2 = optionBase.cloneNode()
+            inpt.type = 'radio'
+            inpt.name = 'theme'
+            inpt.id = inpt.value = labl.htmlFor = optn1.value = optn2.value = theme.id
+            labl.appendChild(document.createTextNode(theme.label))
+            optn1.appendChild(document.createTextNode(theme.label))
+            optn2.appendChild(document.createTextNode(theme.label))
+            radioFragment.appendChild(inpt)
+            radioFragment.appendChild(labl)
+            option1Fragment.appendChild(optn1)
+            option2Fragment.appendChild(optn2)
+        }
+        document.getElementById('theme-group').appendChild(radioFragment)
+        document.getElementById('theme-primary').appendChild(option1Fragment)
+        document.getElementById('theme-secondary').appendChild(option2Fragment)
+    }
+    reflect() {
+        const data = this.settings
+        for(const type in data) {
+            if(typeof data[type] === "object") {
+                this.setState(type, data[type])
+            }
+        }
+    }
+    setState(type, data) {
+        for(const key in data) {
+            Reflector[type](key, data[key])
+        }
+    }
+    wrapper(key, action, func) {
+        const all = document.querySelectorAll(key)
+        for(const item of all) {
+            item.addEventListener(action, (event) => { func(event) })
+        }
+    }
 
-	async setupAlarms() {
-	  console.log(this.settings)
-	  const now = new Date()
-	  console.log(this.formatTime(now))
-	  let t = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1)
-	  console.log(this.formatTime(t))
-	  chrome.alarms.create('adjustment', { 'when': t.getTime() })
-	}
+    async setupAlarms() {
+        console.log(this.settings)
+        const now = new Date()
+        console.log(this.formatTime(now))
+        let t = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1)
+        console.log(this.formatTime(t))
+        chrome.alarms.create('adjustment', { 'when': t.getTime() })
+    }
 
-	addElementsEventListener() {
-		this.wrapper('#save-settings', 'click', (event) => {
-			this.saveData()
-			chrome.runtime.sendMessage({newtab: 'reload'})
-			chrome.runtime.sendMessage({option: 'reload'})
-			if(this.settings.toggle.tgglAutoTheme){
-				this.autoTheme(),this.setupAlarms()
-			} else {
-				chrome.alarms.clearAll(()=>{console.log('Alarms.clearAll')})
-			}
-			let t = document.getElementById('toast')
-			t.style.transform  = 'translateY(-6rem)'
-			setTimeout((a) => {a.style.transform  = 'translateY(6rem)'}, 2000, t)
-		})
-		this.wrapper('.toggle', 'click', (event) => {
-			event.target.classList.toggle('toggle-on')
-			this.settings.toggle[event.target.id] = event.target.classList.contains('toggle-on')
-		})
-		this.wrapper('.text-input', 'blur', (event) => {
-			this.settings.text[event.target.id] = event.target.value
-		})
-		this.wrapper('input[type="radio"]', 'click', (event) => {
-			this.settings.radio[event.target.name] = event.target.id
-		})
-		this.wrapper('input[type="range"]', 'change', (event) => {
-			this.settings.range[event.target.name] = event.target.value
-			for(const item of document.querySelectorAll(`.${event.target.name}`)) {
-				item.value = event.target.value
-			}
-		})
-		this.wrapper('.text-synchronize-slider', 'change', (event) => {
-			let val = event.target.value;
-			if(!Number.isInteger(val)) {val = Math.round(val)}
-			if(val < 0) {val = 0}
-			else if(val > 24) {val = 24}
+    addElementsEventListener() {
+        this.wrapper('#save-settings', 'click', (event) => {
+            this.saveData()
+            chrome.runtime.sendMessage({ newtab: 'reload' })
+            chrome.runtime.sendMessage({ option: 'reload' })
+            if(this.settings.toggle.tgglAutoTheme) {
+                this.autoTheme(), this.setupAlarms()
+            } else {
+                chrome.alarms.clearAll(() => { console.log('Alarms.clearAll') })
+            }
+            let t = document.getElementById('toast')
+            t.style.transform = 'translateY(-6rem)'
+            setTimeout((a) => { a.style.transform = 'translateY(6rem)' }, 2000, t)
+        })
+        this.wrapper('.toggle', 'click', (event) => {
+            event.target.classList.toggle('toggle-on')
+            this.settings.toggle[event.target.id] = event.target.classList.contains('toggle-on')
+        })
+        this.wrapper('.text-input', 'blur', (event) => {
+            this.settings.text[event.target.id] = event.target.value
+        })
+        this.wrapper('input[type="radio"]', 'click', (event) => {
+            this.settings.radio[event.target.name] = event.target.id
+        })
+        this.wrapper('input[type="range"]', 'change', (event) => {
+            this.settings.range[event.target.name] = event.target.value
+            for(const item of document.querySelectorAll(`.${event.target.name}`)) {
+                item.value = event.target.value
+            }
+        })
+        this.wrapper('.text-synchronize-slider', 'change', (event) => {
+            let val = event.target.value;
+            if(!Number.isInteger(val)) { val = Math.round(val) }
+            if(val < 0) { val = 0 } else if(val > 24) { val = 24 }
 
-			const name = event.target.name
-			this.settings.range[name] = val
-			for(const item of document.querySelectorAll(`.${name}`)) {
-				item.value = val
-			}
-			for(const item of document.querySelectorAll(`#${name}Range`)) {
-				item.value = val
-			}
-		})
-		this.wrapper('select', 'change', (event) => {
-			this.settings.select[event.target.name] = event.target.value
-		})
-	}
+            const name = event.target.name
+            this.settings.range[name] = val
+            for(const item of document.querySelectorAll(`.${name}`)) {
+                item.value = val
+            }
+            for(const item of document.querySelectorAll(`#${name}Range`)) {
+                item.value = val
+            }
+        })
+        this.wrapper('select', 'change', (event) => {
+            this.settings.select[event.target.name] = event.target.value
+        })
+    }
 }
 
 class ExtensionInfo {
-	constructor() {
-		this.versionInfo()
-		this.wrapper('https://api.github.com/repos/Y-Ysss/Hello-NewTab/releases/latest', this.gitReleaseInfo)
-		this.wrapper('https://api.github.com/repos/Y-Ysss/Hello-NewTab/commits', this.gitCommitsInfo)
-	}
-	wrapper(url, func) {
-		fetch(url).then((response) => response.json()).then((data) => {
-			func(data)
-		})
-	}
+    constructor() {
+        this.versionInfo()
+        this.wrapper('https://api.github.com/repos/Y-Ysss/Hello-NewTab/releases/latest', this.gitReleaseInfo)
+        this.wrapper('https://api.github.com/repos/Y-Ysss/Hello-NewTab/commits', this.gitCommitsInfo)
+    }
+    wrapper(url, func) {
+        fetch(url).then((response) => response.json()).then((data) => {
+            func(data)
+        })
+    }
 
-	versionInfo() {
-		const manifestData = chrome.runtime.getManifest();
-		let str = `<div class="content-section"><div class="section-title">Installed Extension</div><div class="section-items-slim"><div class="section-item-text">バージョン : ${manifestData.version}</div></div></div>`
-		document.getElementById('ExtensionInfo').insertAdjacentHTML('beforeend', str);
-	}
+    versionInfo() {
+        const manifestData = chrome.runtime.getManifest();
+        let str = `<div class="content-section"><div class="section-title">Installed Extension</div><div class="section-items-slim"><div class="section-item-text">バージョン : ${manifestData.version}</div></div></div>`
+        document.getElementById('ExtensionInfo').insertAdjacentHTML('beforeend', str);
+    }
 
-	gitReleaseInfo(data) {
-		const manifestData = chrome.runtime.getManifest();
-		let str
-		if(data.message !== undefined) {return}
-		if(manifestData.version !== data.name) {
-			const body = data.body.replace(/#{1,6}(.+?)\r?\n/g, '<span>$1</span><br>')
-			str = `<div class="content-section"><div class="section-title">Latest Release</div><div class="section-items-slim"><div class="section-item-text">バージョン : ${data.name}</div></div><div class="section-items-slim"><div class="section-item-text"><b>What's New</b><br>${body}</div></div><div class="section-items-slim"><div class="section-item-text">URL : <a class="url-text" href="${data.html_url}" target="_blank"></a></div></div></div>`
-			str = str.replace(/\r?\n/g, '<br>')
-			document.getElementById('ExtensionInfo').insertAdjacentHTML('beforeend', str);
-		}
-	}
+    gitReleaseInfo(data) {
+        const manifestData = chrome.runtime.getManifest();
+        let str
+        if(data.message !== undefined) { return }
+        if(manifestData.version !== data.name) {
+            const body = data.body.replace(/#{1,6}(.+?)\r?\n/g, '<span>$1</span><br>')
+            str = `<div class="content-section"><div class="section-title">Latest Release</div><div class="section-items-slim"><div class="section-item-text">バージョン : ${data.name}</div></div><div class="section-items-slim"><div class="section-item-text"><b>What's New</b><br>${body}</div></div><div class="section-items-slim"><div class="section-item-text">URL : <a class="url-text" href="${data.html_url}" target="_blank"></a></div></div></div>`
+            str = str.replace(/\r?\n/g, '<br>')
+            document.getElementById('ExtensionInfo').insertAdjacentHTML('beforeend', str);
+        }
+    }
 
-	gitCommitsInfo(data) {
-		let str = ''
-		for(let i = 0; i < 5; i++) {
-			str += `<div class="content-section"><div class="section-title">${data[i].commit.message}</div><div class="section-items-continuation"><div class="section-item-text">${(data[i].commit.author.date).replace('T', ', ').slice(0, -1)} (UTC)</div></div><div class="section-items"><div class="section-item-text"><a class="url-text" href="${data[i].html_url}" target="_blank"></a></div></div></div>`;
-		}
-		document.getElementById('gitCommitsInfo').insertAdjacentHTML('beforeend', str);
-	}
+    gitCommitsInfo(data) {
+        let str = ''
+        for(let i = 0; i < 5; i++) {
+            str += `<div class="content-section"><div class="section-title">${data[i].commit.message}</div><div class="section-items-continuation"><div class="section-item-text">${(data[i].commit.author.date).replace('T', ', ').slice(0, -1)} (UTC)</div></div><div class="section-items"><div class="section-item-text"><a class="url-text" href="${data[i].html_url}" target="_blank"></a></div></div></div>`;
+        }
+        document.getElementById('gitCommitsInfo').insertAdjacentHTML('beforeend', str);
+    }
 }
 
 const opt = new ReflectSettings()
 const info = new ExtensionInfo()
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if(request.option === 'reload') {
-		window.location.reload()
-	}
+    if(request.option === 'reload') {
+        window.location.reload()
+    }
 });
 
 // chrome.storage.onChanged.addListener((changes) => {
